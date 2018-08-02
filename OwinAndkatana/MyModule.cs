b221a5220ba799c94a3ab4492d5373e55f6dd9f8
maqib <muhammadaqib1831@outlook.com>
2018-08-02 12:19:42 +0500

    First test sample

---
+++ OwinAndkatana/MyModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OwinAndkatana
+{
+    public class MyModule : IHttpModule
+    {
+        public void Dispose()
+        {
+           // throw new NotImplementedException();
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.AuthenticateRequest += ctx_AuthRequest;
+        }
+
+        private void ctx_AuthRequest(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
---
+++ OwinAndkatana/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+[assembly: OwinStartup(typeof(OwinAndkatana.Startup))]
+namespace OwinAndkatana
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //xample runs in the ASP.NET request pipeline, which uses System.Web in the context of IIS
+            //app.Run(context =>
+            //{
+            //    context.Response.ContentType = "text/plain";
+            //    return context.Response.WriteAsync("Helloworld");
+            //});
+            app.Use((context, next) =>
+            {
+                PrintCurrentIntegratedPipelineStage(context, "Middleware 1");
+                return next.Invoke();
+            });
+            app.Use((context, next) =>
+            {
+                PrintCurrentIntegratedPipelineStage(context, "2nd MW");
+                return next.Invoke();
+            });
+            app.Run(context =>
+            {
+                PrintCurrentIntegratedPipelineStage(context, "3rd MW");
+                return context.Response.WriteAsync("Hello world");
+            });
+        }
+
+        private void PrintCurrentIntegratedPipelineStage(IOwinContext context, string v)
+        {
+            var currentIntegratedpipelineStage = HttpContext.Current.CurrentNotification;
+            context.Get<TextWriter>("host.TraceOutput").WriteLine(
+                "Current IIS event: " + currentIntegratedpipelineStage
+                + " Msg: " + v);
+        }
+    }
+}
